09ed63de92cd8d74e4f7b816fa6c50ad7d64b8dd
tchanhtinh <tchanhtinh@gmail.com>
2020-08-15 12:26:33 +0700

    [tchanhtinh] Build version 0.1

---
+++ app/precache-manifest.4175a8b38bf998207b41c09ef9f3fb07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68316738c1c8eebe4c4c9c673e2f53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0f726de695ab179d9a",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "2b6130d47b3a0a881e99",
+    "url": "/static/css/main.e70bd433.chunk.css"
+  },
+  {
+    "revision": "9a0f726de695ab179d9a",
+    "url": "/static/js/2.3fd180e3.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.3fd180e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6130d47b3a0a881e99",
+    "url": "/static/js/main.1d2e684c.chunk.js"
+  },
+  {
+    "revision": "29f795faac286aa7a570",
+    "url": "/static/js/runtime-main.d14e497d.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const cryptojs = require('crypto-js')
 const app = express()
 const PORT = 9000
+
+app.use(express.static('app'))
+
+app.get('/', (req, res) => {
+    res.sendFile('/index.html')
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Host is up at localhost:${PORT}`)
